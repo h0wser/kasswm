fade1c24c6c614b7f639a6b25dcb807f4c1f97a8
Simon Molzer <evilhowser@gmail.com>
2015-04-16 15:23:07 +0200

    Added test script

---
+++ test.sh
@@ -0,0 +1,31 @@
+#/bin/bash
+
+# Sup
+# This script runs Xephyr and restarts build/kasswm on changes (compiles) 
+# To simplify testing
+
+BIN_PATH="build/kasswm"
+RES="800x600"
+
+Xephyr -screen $RES -br :1 &
+sleep 1
+
+while true;
+do
+	ATIME=$(stat -c %Z $BIN_PATH)
+
+	if [[ "$ATIME" != "$LTIME" ]]
+	then
+		if [[ -n "$PID" ]]
+		then
+			kill $PID
+		fi
+
+		echo "===== Restarting $BIN_PATH ====="
+		DISPLAY=:1 $BIN_PATH &
+		DISPLAY=:1 xsetroot -solid gray
+		PID="$!"
+		LTIME=$ATIME
+	fi
+	sleep 3
+done
